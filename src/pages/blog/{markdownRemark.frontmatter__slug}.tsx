40f22da6a305a4991ab0778606565d49f8e624f2
0307kwon <0307kwon@gmail.com>
2023-02-25 19:31:37 +0900

    feat: add post thumbnail in post contents

--- src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.tsx
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
 import PageLayout from "../../components/pageLayout"
 import "./markdown.css"
@@ -7,7 +8,7 @@ const BlogPostTemplate = ({ data }) => {
   const {
     markdownRemark: {
       html,
-      frontmatter: { title, date },
+      frontmatter: { title, thumbnail, date },
     },
   } = data
 
@@ -18,6 +19,14 @@ const BlogPostTemplate = ({ data }) => {
         itemScope
         itemType="http://schema.org/Article"
       >
+        <div className="relative w-full rounded-2xl overflow-hidden mb-3 max-w-[700px] mx-auto">
+          <div className="w-full pt-[50%]" />
+          <GatsbyImage
+            image={thumbnail.childImageSharp.gatsbyImageData}
+            alt={`${title} thumbnail`}
+            className="absolute w-full top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
         <section
           dangerouslySetInnerHTML={{ __html: html }}
           itemProp="articleBody"
@@ -37,6 +46,11 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
+        thumbnail {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
         slug
         title
       }
