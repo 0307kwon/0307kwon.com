d1efb2737b67258aaa82c7870d8d6d906775cf93
0307kwon <0307kwon@gmail.com>
2023-02-26 19:04:17 +0900

    feat: change postCard area to link

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "gatsby"
+import { navigate } from "gatsby"
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image"
 import React from "react"
 
@@ -10,23 +10,28 @@ interface Props {
 }
 
 const PostCard = ({ linkTo, thumbnail, title, description }: Props) => {
+  const onLinkTo = () => {
+    navigate(linkTo)
+  }
+
   return (
-    <article className="bg-gray400 rounded-md flex h-24 p-2 gap-3">
+    <article
+      className="bg-gray400 rounded-md flex h-24 p-2 gap-3 postCard"
+      onClick={onLinkTo}
+    >
       <div className="bg-white min-w-[80px] w-[80px] rounded-md relative overflow-hidden">
         <div className="w-full pt-[100%]" />
         <GatsbyImage
           image={thumbnail}
           alt={`${title} thumbnail`}
-          className="absolute w-full h-full top-0 left-1/2 -translate-x-1/2"
+          className="absolute w-full h-full top-0"
         />
       </div>
       <div className="flex flex-col relative w-full">
-        <Link to={linkTo}>
-          <h3 className="text-lg">{title}</h3>
-          <p className="block text-sm max-h-[60%] w-full overflow-hidden text-ellipsis break-keep">
-            {description}
-          </p>
-        </Link>
+        <h3 className="text-lg">{title}</h3>
+        <p className="block text-sm max-h-[60%] w-full overflow-hidden text-ellipsis break-keep">
+          {description}
+        </p>
       </div>
     </article>
   )
